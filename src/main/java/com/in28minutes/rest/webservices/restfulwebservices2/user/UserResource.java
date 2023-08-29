79d8c38d18be94086e46f493260ac9fa667916f9
tomwtomw <krystynajw@yahoo.com>
2023-08-29 14:19:13 -0500

    139 step 09 end of chapter

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices2/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices2/user/UserResource.java
@@ -1,9 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebservices2.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,10 @@ public class UserResource {
     @GetMapping("/users/{userid}")
     public User retrieveUserById(@PathVariable Integer userid) {
         return userDaoService.findOne(userid);
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return userDaoService.save(user);
     }
 }
