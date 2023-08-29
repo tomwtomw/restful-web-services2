0ba82353bd248d1f45071c259f5cf2f319c4142f
tomwtomw <krystynajw@yahoo.com>
2023-08-29 14:33:43 -0500

    140 step 10 end of chapter

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices2/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices2/user/UserResource.java
@@ -1,8 +1,11 @@
 package com.in28minutes.rest.webservices.restfulwebservices2.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,7 +24,13 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public User createUser(@RequestBody User user) {
-        return userDaoService.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser= userDaoService.save(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
