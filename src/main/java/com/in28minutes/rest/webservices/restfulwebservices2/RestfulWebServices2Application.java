b1af6c3500d08f9a813913a9e2d11df064ac5242
tomwtomw <krystynajw@yahoo.com>
2023-08-29 10:40:38 -0500

    139 step 01 Beginning of chaprter

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices2/RestfulWebServices2Application.java
@@ -0,0 +1,13 @@
+package com.in28minutes.rest.webservices.restfulwebservices2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServices2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServices2Application.class, args);
+	}
+
+}
