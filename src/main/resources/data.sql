00ade74d58733c0be369ebc1ebb588e9ee1b2e41
tomwtomw <krystynajw@yahoo.com>
2023-08-30 15:59:56 -0500

    172 step 35 end of chapter

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into user_details (id,birth_date,name) values (1001,current_date(),'Ranga');
-insert into user_details (id,birth_date,name) values (1002,current_date(),'Bolek');
-insert into user_details (id,birth_date,name) values (1003,current_date(),'Lolek');
-
-insert into post (id,description,user_id) values(20001,'learn aws',1001);
-insert into post (id,description,user_id) values(20002,'learn aws2',1001);
-insert into post (id,description,user_id) values(20003,'learn aws3',1002);
+-- insert into user_details (id,birth_date,name) values (1001,current_date(),'Ranga');
+-- insert into user_details (id,birth_date,name) values (1002,current_date(),'Bolek');
+-- insert into user_details (id,birth_date,name) values (1003,current_date(),'Lolek');
+--
+-- insert into post (id,description,user_id) values(20001,'learn aws',1001);
+-- insert into post (id,description,user_id) values(20002,'learn aws2',1001);
+-- insert into post (id,description,user_id) values(20003,'learn aws3',1002);
